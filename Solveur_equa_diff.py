7ce980b5ac0dc562ffe5d98f5ea76dedd58d495b
Benj74100 <59203713+Benj74100@users.noreply.github.com>
2020-04-07 15:43:49 +0200

    Add files via upload

--- Solveur_equa_diff.py
+++ Solveur_equa_diff.py
@@ -37,69 +37,69 @@ def resol(a,b,c,A):
 		
 	deg_sol = degree(A) #degré de la solution
 
-	if d > 0 :
-		x_1 = (-1*b + -1*sqrt(d))/(2*a) and x_2 = (-1*b + sqrt(d))/(2*a)
-		res = C_1*exp(x_1*x) + C_2exp(x_2*x) #solution homogène
-				
-		if A == k*exp(k_1*x):
-			res += L*exp(k_1*x)*poly(deg_sol) 
+		if d > 0 :
+			x_1 = (-1*b + -1*sqrt(d))/(2*a) and x_2 = (-1*b + sqrt(d))/(2*a)
+			res = C_1*exp(x_1*x) + C_2exp(x_2*x) #solution homogène
+					
+			if A == k*exp(k_1*x): #Condition solution particulière
+				res += L*exp(k_1*x)*poly(deg_sol) 
 
-		elif A == k*sin(w*x) or k*cos(w*x):
-			res += L*sin(w*x) + B_*cos(w*x)
-				
-		elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
-			deg_sol = degree(A) 
-			res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
-		else:
-			if c != 0:
-				res += poly(deg_sol)
-			elif c == 0 and b != 0:
-				res += poly(deg_sol + 1)
-			elif b == c == 0:
-				res += poly(deg_sol + 2)
+			elif A == k*sin(w*x) or k*cos(w*x):
+				res += L*sin(w*x) + B_*cos(w*x)
+					
+			elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
+				deg_sol = degree(A) 
+				res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
 			else:
-				print("Error")
-	elif d == 0 :
+				if c != 0:
+					res += poly(deg_sol)
+				elif c == 0 and b != 0:
+					res += poly(deg_sol + 1)
+				elif b == c == 0:
+					res += poly(deg_sol + 2)
+				else:
+					print("Error")
+		elif d == 0 :
 
-		x_1 = (-1*b)/(2*a)
-		res = exp(x_1*x)*(C_1*x + C_2) #Solution homogène
-				
-		if A == k*exp(k_1*x):
-			res += L*exp(k_1*x)*poly(deg_sol)
-		elif A == k*sin(w*x) or k*cos(w*x):
-			res += L*sin(w*x) + B_*cos(w*x)
-		elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
-			res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
-		else:
-			if c != 0:
-				res += poly(deg_sol)
-			elif c == 0 and b != 0:
-				res += poly(deg_sol + 1)
-			elif b == c == 0:
-				res += poly(deg_sol + 2)
+			x_1 = (-1*b)/(2*a)
+			res = exp(x_1*x)*(C_1*x + C_2) #Solution homogène
+					
+			if A == k*exp(k_1*x):#Condition solution particulière
+				res += L*exp(k_1*x)*poly(deg_sol)
+			elif A == k*sin(w*x) or k*cos(w*x):
+				res += L*sin(w*x) + B_*cos(w*x)
+			elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
+				res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
 			else:
-				print("Error")
-	elif d < 0:
-				   
-		x_1 = -1 * b -I*sqrt(-1*d)/(2*a) and x_2 = (-1*b + I*sqrt(-1*d))/(2*a)
-		res = exp(re(x_1)*x)*(C_1*cos(im(X_1*x)) + C_2*sin(im(x_2*x))) #Solution homogène
-				
-		if A == k*exp(k_1*x):
-			res += L*exp(k_1*x)*poly(deg_sol)
-		elif A == k*sin(w*x) or k*cos(w*x):
-			res += L*sin(w*x) + B_*cos(w*x)
-		elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
-			res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
-		else:
-			if c != 0:
-				res += poly(deg_sol)
-			elif c == 0 and b != 0:
-				res += poly(deg_sol + 1)
-			elif b == c == 0:
-				res += poly(deg_sol + 2)
+				if c != 0:
+					res += poly(deg_sol)
+				elif c == 0 and b != 0:
+					res += poly(deg_sol + 1)
+				elif b == c == 0:
+					res += poly(deg_sol + 2)
+				else:
+					print("Error")
+		elif d < 0:
+					   
+			x_1 = -1 * b -I*sqrt(-1*d)/(2*a) and x_2 = (-1*b + I*sqrt(-1*d))/(2*a)
+			res = exp(re(x_1)*x)*(C_1*cos(im(X_1*x)) + C_2*sin(im(x_2*x))) #Solution homogène
+					
+			if A == k*exp(k_1*x):#Condition solution particulière
+				res += L*exp(k_1*x)*poly(deg_sol)
+			elif A == k*sin(w*x) or k*cos(w*x):
+				res += L*sin(w*x) + B_*cos(w*x)
+			elif A == exp(k_1*x)*(L*sin(w*x) + M*cos(w*x)):
+				res += poly(deg_sol)*exp(k_1*x)*(L*sin(w*x) + M*cos(w*x))
 			else:
-				print("Error")
-										
-				   
-	return res
+				if c != 0:
+					res += poly(deg_sol)
+				elif c == 0 and b != 0:
+					res += poly(deg_sol + 1)
+				elif b == c == 0:
+					res += poly(deg_sol + 2)
+				else:
+					print("Error")
+											
+					   
+		return res
 
